236a27fb681a694b0fe21f116f5eaaee98258d51
KidUnion <KidUnion@outlook.com>
2025-06-29 02:40:02 +0800

    完成[yi]赵云，添加[qun]地铁

--- yi.lua
+++ yi.lua
@@ -392,9 +392,9 @@ Fk:loadTranslationTable{
   ["designer:yi__wangping"] = "KidUnion",
   ["yi__feijun"] = "飞军",
   [":yi__feijun"] = "每回合每种花色限一次，准备阶段结束时，你可弃置任意张花色相同的牌并视为使用一张无距离限制的【杀】/将手牌摸至体力上限，"..
-  "然后此技能的发动时机提前/延后至多等量个阶段",
+  "然后此技能的发动时机提前/延后至多等量个阶段。",
   ["yi__binglue"] = "兵略",
-  [":yi__binglue"] = "你的回合结束时，你可执行本回合的第X个阶段（X为本回合进入弃牌堆的花色数）",
+  [":yi__binglue"] = "你的回合结束时，你可执行本回合的第X个阶段。（X为本回合进入弃牌堆的花色数）",
   ["@[yi__feijun]"] = "飞军",
   ["yi__feijun_active"] = "飞军",
   ["@yi__feijun-turn"] = "飞军",
@@ -412,6 +412,74 @@ Fk:loadTranslationTable{
   ["$yi__binglue1"] = "奇略兵速，敌未能料之。",
   ["$yi__binglue2"] = "兵略者，明战胜攻取之数，形机之势，诈谲之变。",
   ["~yi__wangping"] = "无当飞军，也有困于深林之时……",
+}
+
+-- 赵云
+local zhaoyun = General(extension, "yi__zhaoyun", "shu", 4)
+local longdan = fk.CreateViewAsSkill{
+  name = "yi__longdan",
+  anim_type = "switch",
+  switch_skill_name = "yi__longdan",
+  prompt = function(self)
+    return "#yi__longdan_choose-"..Self:getSwitchSkillState(self.name, false, true)
+  end,
+  pattern = ".|.|.|.|.|basic",
+  interaction = function()
+    local all_names = U.getAllCardNames("b", true)
+    local names = U.getViewAsCardNames(Self, "shizong", all_names)
+    if #names == 0 then return false end
+    return UI.ComboBox { choices = names, all_choices = all_names }
+  end,
+  card_filter = Util.FalseFunc,
+  view_as = function(self)
+    if not self.interaction.data then return end
+    local card = Fk:cloneCard(self.interaction.data)
+    card.skillName = self.name
+    return card
+  end,
+  before_use = function(self, player, use)
+    local room = player.room
+    if player:getSwitchSkillState(self.name, false) == fk.SwitchYin then
+      local card = room:askForDiscard(player, 1, 1, true, self.name,
+       false, ".", "#yi__longdan-yin:::"..use.card.name, false)
+      if #card > 0 then return end
+    elseif player:getSwitchSkillState(self.name, false) == fk.SwitchYang then
+      room:addTableMark(player, "@yi__longdan-turn", use.card.name)
+      room:drawCards(player, 1, self.name)
+    end
+    return self.name
+  end,
+  enabled_at_play = function(self, player)
+    return true
+  end,
+  enabled_at_response = function(self, player, response)
+    return not response
+  end,
+}
+local longdan_prohibit = fk.CreateProhibitSkill{
+  name = "#yi__longdan_prohibit",
+  prohibit_use = function(self, player, card)
+    return table.contains(player:getTableMark("@yi__longdan-turn"), card.trueName)
+  end,
+}
+longdan:addRelatedSkill(longdan_prohibit)
+zhaoyun:addSkill(longdan)
+Fk:loadTranslationTable{
+  ["yi__zhaoyun"] = "异赵云",
+  ["#yi__zhaoyun"] = "虎将龙胆",
+  ["designer:yi__zhaoyun"] = "KidUnion",
+  ["yi__longdan"] = "龙胆",
+  [":yi__longdan"] = "转换技，当你需要使用基本牌时，阴：你可弃一张牌并视为使用之；阳：你可摸一张牌并令你本回合不能使用此牌名的牌。",
+  ["#yi__longdan_choose-yang"] = "龙胆：选择一种基本牌，弃一张牌并视为使用之。",
+  ["#yi__longdan_choose-yin"] = "龙胆：选择一种基本牌，摸一张牌并令你本回合不能使用之。",
+  ["#yi__longdan-yin"] = "弃置一张牌并视为使用%arg",
+  ["#yi__longdan_prohibit"] = "龙胆",
+  ["@yi__longdan-turn"] = "龙胆",
+  
+  ["$yi__longdan1"] = "保驾扶危主，冲阵透重围！",
+  ["$yi__longdan2"] = "银枪映豪胆，赤血鉴忠心！",
+
+  ["~yi__zhaoyun"] = "生驱单骑摧敌锐，死作忠魂佑主周！",
 }
 
 return extension
